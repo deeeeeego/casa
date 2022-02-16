a6ad58cd575a339e52e13317c8303a6c8f0e9213
deeeeeego <95698958+deeeeeego@users.noreply.github.com>
2022-02-16 17:16:07 -0600

    Add files via upload

---
+++ precache-manifest.ad6422bf039cf736db85fd12f382a826.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a6ce6573d9979304cecf83849f0b9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa18fbad036d4abcfebc",
+    "url": "./static/css/main.3c548d88.chunk.css"
+  },
+  {
+    "revision": "37af338a6f423b4a34d0",
+    "url": "./static/js/2.af0e83db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.af0e83db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa18fbad036d4abcfebc",
+    "url": "./static/js/main.9409d337.chunk.js"
+  },
+  {
+    "revision": "8176c60e066153806f52",
+    "url": "./static/js/runtime-main.bc88bcec.js"
+  },
+  {
+    "revision": "6214fef263bf351d32d599371642be62",
+    "url": "./static/media/FiraCode-Light.6214fef2.woff"
+  },
+  {
+    "revision": "3da66ca1931ea28aafb1c6e9cdf1d0f8",
+    "url": "./static/media/FiraCode-Regular.3da66ca1.woff"
+  },
+  {
+    "revision": "e5067b7c3b648f5f8fbeb3288b6d0124",
+    "url": "./static/media/duckduckgo.e5067b7c.svg"
+  },
+  {
+    "revision": "9ef22081c78065f1bdeb54de36e6d6cf",
+    "url": "./static/media/google.9ef22081.svg"
+  },
+  {
+    "revision": "06ef25683d4f965ec5c9f7041f277950",
+    "url": "./static/media/logo.06ef2568.png"
+  },
+  {
+    "revision": "93a4a519b6971022bff0f702faa021bc",
+    "url": "./static/media/pic_1.93a4a519.jpg"
+  },
+  {
+    "revision": "83888571e6aec211a85144b9dfb02a0e",
+    "url": "./static/media/pic_2.83888571.jpg"
+  },
+  {
+    "revision": "12b8ec1ab4895a0cc6e268774fc4c0dd",
+    "url": "./static/media/pic_3.12b8ec1a.jpg"
+  },
+  {
+    "revision": "3fbc8a94e19d9db05ac35a4bf49807eb",
+    "url": "./static/media/pic_4.3fbc8a94.jpg"
+  },
+  {
+    "revision": "650b227119fa64d54aef3f06b09ebfb8",
+    "url": "./static/media/pic_5.650b2271.png"
+  },
+  {
+    "revision": "743812283900ce7dd1845eff76bb9add",
+    "url": "./static/media/pic_6.74381228.png"
+  },
+  {
+    "revision": "c928d8077ec24d9ff2ec6d8f38fb37c4",
+    "url": "./static/media/pic_7.c928d807.png"
+  },
+  {
+    "revision": "ba1342f42d643e4f554eaf120c6a2a38",
+    "url": "./static/media/pic_8.ba1342f4.png"
+  },
+  {
+    "revision": "f90c346a2cf4f445626a8b1211caa6df",
+    "url": "./static/media/qwant.f90c346a.svg"
+  }
+]);
